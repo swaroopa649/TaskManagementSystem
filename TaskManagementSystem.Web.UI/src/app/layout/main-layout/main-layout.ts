e63cfc303f5b9b6682a5d39d7d5824a695e8e65e
swaroopa649 <kaviriswaroopa1996@gmail.com>
2026-09-24 15:52:06 +0530

    commit

---
+++ TaskManagementSystem.Web.UI/src/app/layout/main-layout/main-layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar';
+import { HeaderComponent } from '../header/header';
+
+@Component({
+  selector: 'app-main-layout',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
+  templateUrl: './main-layout.html',
+  styleUrls: ['./main-layout.css']
+})
+export class MainLayoutComponent {}
